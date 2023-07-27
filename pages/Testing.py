b4e21e02beac4b2295cd43ad309bca80d1632ec3
robsca <66328669+robsca@users.noreply.github.com>
2023-07-27 15:48:05 +0100

    correcting

--- pages/Testing.py
+++ pages/Testing.py
@@ -16,28 +16,30 @@ import streamlit as st
 from streamlit_ace import st_ace
 
 code = '''
-import numpy as np
-import yfinance as yf
-import plotly.graph_objects as go
-options_for_ticker = [
-    'eurusd=x',
-    'gbpusd=x',
-    'usdjpy=x',
-    'usdchf=x',
-    'TSLA',
-    'AAPL',
-    'MSFT',
-    'AMZN',
-    'GOOG',
-    'VUSA.L',
+def function():
+    import numpy as np
+    import yfinance as yf
+    import plotly.graph_objects as go
+    options_for_ticker = [
+        'eurusd=x',
+        'gbpusd=x',
+        'usdjpy=x',
+        'usdchf=x',
+        'TSLA',
+        'AAPL',
+        'MSFT',
+        'AMZN',
+        'GOOG',
+        'VUSA.L',
 
-]
-import datetime
-ticker = st.sidebar.selectbox('Select ticker', options_for_ticker)
-interval = st.sidebar.selectbox('Select interval', ['1d', '1h', '30m', '15m', '5m', '1m'])
-start_date = st.sidebar.date_input('Start date', value=datetime.date(2021, 1, 1))
-end_date = st.sidebar.date_input('End date', value=datetime.date(2021, 7, 1))
-data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    ]
+    import datetime
+    ticker = st.sidebar.selectbox('Select ticker', options_for_ticker)
+    interval = st.sidebar.selectbox('Select interval', ['1d', '1h', '30m', '15m', '5m', '1m'])
+    start_date = st.sidebar.date_input('Start date', value=datetime.date(2021, 1, 1))
+    end_date = st.sidebar.date_input('End date', value=datetime.date(2021, 7, 1))
+    data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    return data
     
 '''
 code = st_ace(value=code, language='python', theme='monokai', keybinding='vscode', font_size=12, tab_size=4, show_gutter=True, show_print_margin=True, wrap=True, auto_update=True, readonly=False, key=None)
