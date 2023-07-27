7fb040ba8442fb8aa135330fd7a2556379a36490
robsca <66328669+robsca@users.noreply.github.com>
2023-07-27 15:52:09 +0100

    add graphs

--- pages/Testing.py
+++ pages/Testing.py
@@ -16,29 +16,39 @@ import streamlit as st
 from streamlit_ace import st_ace
 
 code = '''
-def function():
-    import numpy as np
-    import yfinance as yf
-    import plotly.graph_objects as go
-    options_for_ticker = [
-        'eurusd=x',
-        'gbpusd=x',
-        'usdjpy=x',
-        'usdchf=x',
-        'TSLA',
-        'AAPL',
-        'MSFT',
-        'AMZN',
-        'GOOG',
-        'VUSA.L',
+import yfinance as yf
+import plotly.graph_objects as go
+import datetime
 
-    ]
-    import datetime
-    ticker = st.sidebar.selectbox('Select ticker', options_for_ticker)
-    interval = st.sidebar.selectbox('Select interval', ['1d', '1h', '30m', '15m', '5m', '1m'])
-    start_date = st.sidebar.date_input('Start date', value=datetime.date(2021, 1, 1))
-    end_date = st.sidebar.date_input('End date', value=datetime.date(2021, 7, 1))
+options_for_ticker = [
+    'eurusd=x',
+    'gbpusd=x',
+    'usdjpy=x',
+]
+
+ticker = st.sidebar.selectbox('Select ticker', options_for_ticker)
+interval = st.sidebar.selectbox('Select interval', ['1d', '1h', '30m', '15m', '5m', '1m'])
+start_date = st.sidebar.date_input('Start date', value=datetime.date(2021, 1, 1))
+end_date = st.sidebar.date_input('End date', value=datetime.date(2021, 7, 1))
+
+def function():
+    '''
+    This function will be execute automatically
+    '''
+
     data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    # add plot 
+    fig = go.Figure()
+    fig.add_trace(go.Candlestick(x=data.index,
+                    open=data['Open'],
+                    high=data['High'],
+                    low=data['Low'],
+
+                    close=data['Close'], name = 'market data'))
+    fig.update_layout(
+        title='{} Stock Chart'.format(ticker),
+        yaxis_title='Stock Price (USD per Shares)')
+    st.plotly_chart(fig, use_container_width=True)
     return data
     
 '''
